9af6d5897d210fbb86414ca9ab602d7c0a873fc0
ezaatama <ezatama33@gmail.com>
2025-10-05 17:52:42 +0700

    [update] cors

--- src/application/web.js
+++ src/application/web.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { publicRouter } from '../routes/public_api.js';
 import { errorMiddleware } from '../middleware/error_middleware.js';
 import { routers } from '../routes/api.js';
+import cors from 'cors';
 
 export const web = express();
 web.use(cors({
