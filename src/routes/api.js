e3678ed3cf6c57a52fd6c8457ac681cde16f0e14
ezaatama <ezatama33@gmail.com>
2025-10-05 17:53:00 +0700

    [update[ new routes

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,6 +2,7 @@ import express from "express";
 import atasanController from "../controller/atasan_controller.js"; 
 import memberController from "../controller/member_controller.js";
 import salesController from "../controller/data_reseller_controller.js";
+import dokumenController from "../controller/dokumen_controller.js";
 import { authMiddleware, adminMiddleware } from "../middleware/auth_middleware.js";
 
 const routers = new express.Router();
@@ -9,14 +10,23 @@ const routers = new express.Router();
 //atasan routes
 routers.post('/api/atasan', authMiddleware, adminMiddleware, atasanController.create);
 routers.get('/api/atasan', authMiddleware, adminMiddleware, atasanController.getAll);
-routers.delete('/api/atasan/:m_rep_id', authMiddleware, adminMiddleware, atasanController.deleteAtasan);
+routers.get('/api/atasan/:id', authMiddleware, adminMiddleware, atasanController.getById);
+routers.put('/api/atasan/:id', authMiddleware, adminMiddleware, atasanController.updateAtasan);
+routers.delete('/api/atasan/:id', authMiddleware, adminMiddleware, atasanController.deleteAtasan);
 
 //member routes
 routers.post('/api/member', authMiddleware, adminMiddleware, memberController.create);
 routers.get('/api/member', authMiddleware, adminMiddleware, memberController.getAll);
+routers.get('/api/member/:id', authMiddleware, adminMiddleware, memberController.getById);
+routers.put('/api/member/:id', authMiddleware, adminMiddleware, memberController.updateMember);
+routers.delete('/api/member/:id', authMiddleware, adminMiddleware, memberController.deleteMember);
 
 //sales routes
-routers.get('/api/sales', authMiddleware, adminMiddleware, salesController.getDataSales);
+routers.get('/api/sales', authMiddleware, salesController.getDataSales);
+
+//export excel
+routers.get('/api/export/sales/excel', authMiddleware, adminMiddleware, dokumenController.downloadDataExcel);
+routers.get('/api/export/sales/preview', authMiddleware, adminMiddleware, dokumenController.previewSalesData);
 
 export {
     routers
--- src/routes/public_api.js
+++ src/routes/public_api.js
@@ -2,6 +2,9 @@ import express from 'express';
 import userController from '../controller/user_controller.js';
 
 const publicRouter = new express.Router();
+publicRouter.get("/hello", (req, res) => {
+  res.json({ message: "Hello from API" });
+});
 publicRouter.post('/api/login', userController.login);
 
 export {
